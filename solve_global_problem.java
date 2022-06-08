d5f981b1c9e514d774351fcfbc4e72a88fce0b4d
himgb1 <38461885+himgb1@users.noreply.github.com>
2022-06-08 16:24:19 +0800

    Create solve_global_problem.java
    //2022-06-08
    //create by himgb1
    
    //filename: solve_global_problem.java
    //Description: This is the prototype of solving the global problem using information technology

---
+++ solve_global_problem.java
@@ -0,0 +1,15 @@
+//2022-06-08
+//create by himgb1
+
+//filename: solve_global_problem.java
+//Description: This is the prototype of solving the global problem using information technology
+
+int i=0;
+boolean result=false;
+
+while (exist(global problem)){
+  result = AIProblemSolver(global problem[i]);
+  print("Global problem " + i + " result is" + result);
+  i++;
+}
+
